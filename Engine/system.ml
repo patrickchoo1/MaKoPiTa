109218f2312c51029f273bdfdc7a3dffd57d5692
Koji <kjk227@cornell.edu>
2023-05-02 15:33:42 -0400

    Refined GJK

--- Engine/system.ml
+++ Engine/system.ml
@@ -24,7 +24,6 @@ end
  **********************************************************************)
 
 module VectorMath = struct
-  let components : (module Component.Sig) list = [ (module Vector) ]
   let zero_vec : Vector.s = { vec = (0., 0., 0.) }
 
   let make_vec (x : float) (y : float) (z : float) : Vector.s =
@@ -60,10 +59,6 @@ module VectorMath = struct
 
   let magnitude (v : Vector.s) : float =
     match v.vec with x, y, z -> sqrt ((x ** 2.) +. (y ** 2.) +. (z ** 2.))
-
-  let on_update ids = ()
-
-  include (val System.create on_update components : System.Sig)
 end
 
 module ShapeCollisionDetection = struct
@@ -73,46 +68,32 @@ module ShapeCollisionDetection = struct
     [ (module Position); (module Vector); (module Shape) ]
 
   let support (s1 : Shape.s) (s2 : Shape.s) (d : Vector.s) : Vector.s =
-    match (s1, s2) with
-    | Polygon { verticies = verts1 }, Polygon { verticies = verts2 } ->
-        let p1 =
+    let p1 =
+      match s1 with
+      | Polygon { verticies = verts } ->
           List.fold_left
             (fun acc v -> if dot v d > dot acc d then v else acc)
-            (List.hd verts1) verts1
-        in
-        let p2 =
-          List.fold_left
-            (fun acc v -> if dot v (neg d) > dot acc (neg d) then v else acc)
-            (List.hd verts2) verts2
-        in
-        sub p1 p2
-    | Polygon { verticies = verts1 }, Circle { radius = r2; center = c2 } ->
-        let p1 =
+            (List.hd verts) verts
+      | Circle { radius = r; center = c } ->
+          add c (scale (scale d r) (1. /. magnitude d))
+    in
+
+    let p2 =
+      match s2 with
+      | Polygon { verticies = verts } ->
           List.fold_left
             (fun acc v -> if dot v d > dot acc d then v else acc)
-            (List.hd verts1) verts1
-        in
-        let p2 = add c2 (scale (scale d r2) (1. /. magnitude d)) in
-        sub p1 p2
-    | Circle { radius = r1; center = c1 }, Polygon { verticies = verts2 } ->
-        let p1 = add c1 (scale (scale d r1) (1. /. magnitude d)) in
-        let p2 =
-          List.fold_left
-            (fun acc v -> if dot v (neg d) > dot acc (neg d) then v else acc)
-            (List.hd verts2) verts2
-        in
-        sub p1 p2
-    | Circle { radius = r1; center = c1 }, Circle { radius = r2; center = c2 }
-      ->
-        let p1 = add c1 (scale (scale d r1) (1. /. magnitude d)) in
-        let p2 = add c2 (scale (scale d r2) (1. /. magnitude d)) in
-        sub p1 p2
+            (List.hd verts) verts
+      | Circle { radius = r; center = c } ->
+          add c (scale (scale d r) (1. /. magnitude d))
+    in
+    sub p1 p2
 
   let line_case simplex d =
     match !simplex with
     | [ b; a ] ->
         let ab, ao = (sub b a, neg a) in
-        let ab_perp = cross ab ao |> cross ab in
+        let ab_perp = cross (cross ab ao) ab in
         d := ab_perp;
         if ab_perp = zero_vec then true else false
     | _ -> failwith "Invalid Line Simplex"
@@ -121,8 +102,8 @@ module ShapeCollisionDetection = struct
     match !simplex with
     | [ c; b; a ] ->
         let ab, ac, ao = (sub b a, sub c a, neg a) in
-        let ab_perp = cross ac ab |> cross ab in
-        let ac_perp = cross ab ac |> cross ac in
+        let ab_perp = cross (cross ac ab) ab in
+        let ac_perp = cross (cross ab ac) ac in
         if dot ab_perp ao > 0. then (
           d := ab_perp;
           simplex := [ b; a ];
